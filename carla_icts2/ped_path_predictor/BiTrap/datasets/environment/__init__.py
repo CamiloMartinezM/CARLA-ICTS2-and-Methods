91c8fb1d90ad8aaf9917cc385472aa380a000b52
Camilo Martínez <cama00005@stud.uni-saarland.de>
2025-06-03 22:34:02 +0200

    Removed unnecessary stuff (only video-related left)
    * Removed SAV, path predictors, etc.
    * Renamed `extract_pp_data.py` to `main.py`.

--- carla_icts2/ped_path_predictor/BiTrap/datasets/environment/__init__.py
+++
@@ -1,8 +0,0 @@
-from .data_structures import DoubleHeaderNumpyArray, RingBuffer, SingleHeaderNumpyArray
-from .data_utils import derivative_of
-from .environment import Environment
-from .node import Node
-from .node_type import NodeTypeEnum
-from .scene import Scene
-from .scene_graph import SceneGraph, TemporalSceneGraph
-# from .map import GeometricMap
